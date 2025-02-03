251cb054ae92b6a38807bf878cfd2f50e2710ffb
Direcx <mel.langfeld@gmx.de>
2025-02-03 03:31:11 +0100

    updated parsing of example data

--- ExcelConverter.py
+++ ExcelConverter.py
@@ -1,6 +1,7 @@
 from typing import Dict, List, Set, Tuple
 import logging
 
+#TODO: read workshop capacities from excel file/sheet
 WORKSHOP_CAPACITIES = {
     "A" : 3,
     "B" : 3,
@@ -8,60 +9,59 @@ WORKSHOP_CAPACITIES = {
     "D" : 3
 }
 
-PRIORITY_SCORES = [3, 2, 1]
+HIGHEST_PREFERENCE_PRIO = 10
 
-def parse_input_data(data: str) -> List[List[str]]:
+class Person:
+    name = ""
+    preferences = set() # dictionary with "workshop -> prio of pref"
+    friend = ""
+    excluded_pref = ""
+    assigned_workshops = {}
+
+    def __init__(self, row: List[str]):
+        self.name = row[1]
+        self.friend = row[2]
+        self.excluded_pref = row[3]
+        for i in range(4):
+            if i + 4 < len(row): # check if index exists
+                self.preferences.add(HIGHEST_PREFERENCE_PRIO - i)
+
+# results of reading form Excel sheet:
+# grouped_persons,
+# preferences of grouped_persons,
+# excluded_prefs of grouped_persons,
+# workshops with capacities and categories
+
+PRIORITY_SCORES = [4, 3, 2, 1]
+EXCLUDE = [1]
+
+
+#TODO: read persons data from excel file/sheet
+def parse_persons_data(data: str) -> Dict[str, Person]:
     """Parse the input data from Excel."""
     logging.info("Parsing input data")
     lines = data.strip().split('\n')
-    return [line.split('\t') for line in lines[1:]]  # Skip header
+    lines_of_data = [line.split('\t') for line in lines[1:]] # Skip header
 
-
-def process_survey_data(data: List[List[str]]) -> Tuple[Dict[str, List[str]], List[str], Set[str]]:
-    """Process survey data to get preferences, friend pairs, and selected shifts."""
-    logging.info("Processing survey data")
-    preferences = {}
-    friend_pairs = {}
-    selected_workshops = set()
-
-    for row in data:
-        if len(row) < 3:  # Skip rows with insufficient data
+    # polish data from lines of strings to usable objects
+    persons_data: dict[str, Person]= {}
+    for row in lines_of_data:
+        if len(row) < 8:
             continue
-        name = row[1]
-        friends_string = row[2]
-        preferences[name] = []
+        person = Person(row)
+        persons_data[person.name] = person
 
-        # Store preferences and track selected shifts
-        for i in range(3):
-            if i + 3 < len(row):  # Check if the index exists
-                shift = row[i + 3]
-                if shift and shift in WORKSHOP_CAPACITIES:
-                    preferences[name].append(shift)
-                    selected_workshops.add(shift)
-    return preferences, friends_string, selected_workshops
+    return persons_data
+
+
 
 excel_data = """
-    Zeitstempel	Dein Name	Personen, mit denen du am liebsten eine Schicht zusammen machen würdest. Hier kannst du so viele Personen auswählen, wie du möchtest.	Priorität 1	Priorität 2	Priorität 3	Priorität 4	Priorität 5
-    08.09.2024 23:52:47	Mark	Anna, Johanna, Liv, Melwin, Niki, Sarah K, Sarah R, Tobi J, Tobi L	Bar 23:00-00:30	Bar 00:30-02:00	Kasse 23:00-00:30	Einlass 23:00-00:30	
-    08.09.2024 23:53:57	Julian	Anna, Finn, Flori, Magda, Mark, Niki, Sarah K, Sarah R, Tobi J, Tobi L	Bar 21:30-23:00	Bar 23:00-00:30	Kasse 19:45-21:30	Kasse 21:30-23:00	Bar 00:30-02:00
-    08.09.2024 23:57:04	Luca	Lisa, Sarah D, Toto	Kasse 19:45-21:30	Bar 19:45-21:30	Kasse 21:30-23:00	Einlass 19:45-21:30	Bar 21:30-23:00
-    08.09.2024 23:57:07	Lisa	Luca, Sarah D, Toto	Kasse 19:45-21:30	Bar 19:45-21:30	Kasse 21:30-23:00	Einlass 19:45-21:30	Bar 21:30-23:00
-    08.09.2024 23:57:22	Johanna	Liv, Magda, Mark, Melwin, Sarah K, Sarah R, Tobi J, Tobi L	Bar 19:45-21:30	Einlass 19:45-21:30	Kasse 19:45-21:30	Bar 21:30-23:00	Einlass 21:30-23:00
-    09.09.2024 00:01:26	Melwin	Liv	Bar 21:30-23:00	Bar 23:00-00:30	Kasse 21:30-23:00		
-    09.09.2024 00:01:44	Liv	Melwin	Bar 21:30-23:00	Bar 23:00-00:30	Kasse 21:30-23:00		
-    09.09.2024 00:03:39	Anna	Flori, Mark, Sarah K, Sarah R, Tobi J	Bar 21:30-23:00	Bar 23:00-00:30	Kasse 21:30-23:00	Kasse 23:00-00:30	Bar 19:45-21:30
-    09.09.2024 00:04:59	Finn	Alex, Anna, Elias, Flori, Isabell, Johanna, Julian, Lenny, Linda, Lisa, Liv, Luca, Magda, Mark, Melwin, Niki, Paula, Sarah D, Sarah K, Sarah R, Sophia, Tobi J, Tobi L, Toto	Bar 23:00-00:30	Bar 00:30-02:00	Bar 21:30-23:00	Bar 19:45-21:30	
-    09.09.2024 08:01:09	Sarah D	Lisa, Luca, Toto	Kasse 19:45-21:30	Einlass 19:45-21:30	Bar 19:45-21:30		
-    09.09.2024 10:08:16	Toto	Sarah D	Kasse 19:45-21:30	Einlass 19:45-21:30	Bar 19:45-21:30	Kasse 19:45-21:30	Einlass 19:45-21:30
-    09.09.2024 10:40:30	Magda	Julian, Tobi L	Bar 21:30-23:00	Bar 19:45-21:30	Kasse 19:45-21:30		
-    09.09.2024 10:53:14	Alex	Anna, Finn, Flori, Julian, Magda, Mark, Niki, Sarah K, Sarah R, Tobi J, Tobi L	Bar 21:30-23:00	Bar 19:45-21:30	Bar 23:00-00:30		
-    09.09.2024 12:37:22	Niki	Julian, Liv, Mark, Sarah K, Sarah R, Tobi L	Bar 21:30-23:00	Einlass 21:30-23:00	Einlass 23:00-00:30	Bar 19:45-21:30	Bar 23:00-00:30
-    09.09.2024 12:44:28	Isabell	Paula	Einlass 19:45-21:30	Einlass 21:30-23:00	Einlass 23:00-00:30		
-    09.09.2024 12:44:47	Paula	Isabell	Einlass 19:45-21:30	Einlass 21:30-23:00	Einlass 23:00-00:30		
-    09.09.2024 12:49:51	Linda	Lenny, Lisa, Liv, Melwin, Toto	Bar 19:45-21:30	Kasse 19:45-21:30	Einlass 19:45-21:30		
-    09.09.2024 13:35:32	Elias	Alex, Anna, Finn, Flori, Isabell, Johanna, Julian, Lenny, Linda, Lisa, Liv, Luca, Magda, Mark, Melwin, Niki, Paula, Sarah D, Sarah K, Sarah R, Sophia, Tobi J, Tobi L, Toto	Bar 19:45-21:30	Bar 21:30-23:00	Einlass 19:45-21:30	Einlass 21:30-23:00	Kasse 19:45-21:30
-    09.09.2024 16:16:27	Sophia	Lenny, Linda, Liv, Melwin	Einlass 19:45-21:30	Bar 19:45-21:30	Kasse 19:45-21:30	Einlass 21:30-23:00	Kasse 21:30-23:00
-    09.09.2024 17:37:57	Sarah K	Anna, Flori, Johanna, Julian, Mark, Melwin, Niki, Sophia, Tobi J, Tobi L	Bar 21:30-23:00	Einlass 19:45-21:30	Kasse 19:45-21:30	Einlass 21:30-23:00	Kasse 21:30-23:00
-    09.09.2024 20:42:56	Tobi L	Julian, Magda	Bar 21:30-23:00	Bar 19:45-21:30	Kasse 19:45-21:30		
-    09.09.2024 21:17:18	Tobi J	Anna, Finn, Flori, Johanna, Julian, Magda, Mark, Melwin, Niki, Sarah K, Sarah R, Tobi L	Bar 21:30-23:00	Bar 23:00-00:30	Kasse 21:30-23:00		
-    """
+    Kopfzeile
+01	Anna	Beate	A	B	C	C	B
+02	Beate	Anna	A	B	C	C	B
+03	Chris	Dieter	B	C	A	A	C
+04	Dieter	Emil	B	C	A	A	C
+05	Emil	Chris	B	C	A	A	C
+06	Fred	 	C	A	D	B	D
+07	Giesela	 	D	A	C	C	B
+"""
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import logging
-import ExcelConverter
+import ExcelConverter as ec
 from ExcelConverter import excel_data
 
 # Set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 if __name__ == "__main__":
-    print(excel_data)
+    print(ec.parse_persons_data(excel_data))
